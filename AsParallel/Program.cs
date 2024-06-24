6ccca1863c60efda1825b0ae04f8446190955101
sahinmaral <sahin.maral@hotmail.com>
2024-06-25 01:20:01 +0300

    Done course

---
+++ AsParallel/Program.cs
@@ -0,0 +1,39 @@
+﻿bool ExampleProcess(int number)
+{
+    return number % 2 == 0;
+}
+
+void FilteringWithoutParallel(List<int> array)
+{
+    array.Where(ExampleProcess).ToList().ForEach((number) =>
+    {
+        Console.WriteLine($"Number : {number}");
+    });
+}
+
+void FilteringWithParallel(List<int> array)
+{
+    array.AsParallel().Where(ExampleProcess).ToList().ForEach((number) =>
+    {
+        Console.WriteLine($"Number : {number}");
+    });
+
+
+}
+
+void FilteringWithParallel_V2(List<int> array)
+{
+    var newArray = array.AsParallel().Where(ExampleProcess);
+    newArray.ForAll((number) =>
+    {
+        Console.WriteLine($"Number : {number}");
+    });
+}
+
+
+var array = Enumerable.Range(0, 100).ToList();
+
+FilteringWithParallel_V2(array);
+
+
+
---
+++ PLINQExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using PLINQExample.Models;
+
+AdventureWorks2022Context context = new AdventureWorks2022Context();
+
+var products = (from p in context.Products.AsParallel()
+               where p.ListPrice > 10M
+               select p).Take(10);
+
+products.ForAll((product) =>
+{
+    Console.WriteLine(product.Name);
+});
+
+
